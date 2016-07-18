7c2c665da8ea2b89fa99b7a06d00a08e85f8b9f0
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2016-07-18 23:19:27 +0200

    feat: Started implementing Matrix4

---
+++ src/Matrix4.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <array>
+
+namespace Phoenix
+{
+	class Matrix4
+	{
+	private:
+		using MatrixData = std::array<std::array<float, 4>, 4>;
+		MatrixData m_data;
+
+	public:
+		Matrix4()
+			: m_data(MatrixData{})
+		{}
+
+		Matrix4(float m00, float m01, float m02, float m03,
+				float m10, float m11, float m12, float m13,
+				float m20, float m21, float m22, float m23,
+				float m30, float m31, float m32, float m33)
+		{
+			m_data[0][0] = m00;
+			m_data[0][1] = m01;
+			m_data[0][2] = m02;
+			m_data[0][3] = m03;
+			m_data[1][0] = m10;
+			m_data[1][1] = m11;
+			m_data[1][2] = m12;
+			m_data[1][3] = m13;
+			m_data[2][0] = m20;
+			m_data[2][1] = m21;
+			m_data[2][2] = m22;
+			m_data[2][3] = m23;
+			m_data[3][0] = m00;
+			m_data[3][1] = m31;
+			m_data[3][2] = m32;
+			m_data[3][3] = m33;
+		}
+
+	};
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include "Vec3.h"
+#include "Matrix4.h"
 #include <cassert>
 
 int main()
 {
-	using namespace Phoenix;
-	
-	Vec3 a{ 1,0,0 };
-	Vec3 b{ 0,1,0 };
+	Phoenix::Vec3 a{ 1,0,0 };
+	Phoenix::Vec3 b{ 0,1,0 };
 
 	assert(a.dot(b) == 0);
-	assert(Vec3( 0,1,0 ).reflect(Vec3(0.5, 0.5, 0)) == Vec3(-0.5, 0.5, 0));
+	assert(Phoenix::Vec3( 0,1,0 ).reflect(Phoenix::Vec3(0.5, 0.5, 0)) == Phoenix::Vec3(-0.5, 0.5, 0));
 	
+	Phoenix::Matrix4 mat{};
+
 	return 0;
 }
