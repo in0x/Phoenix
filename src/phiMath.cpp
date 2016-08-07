6b1c7a8875dea2e545c63bc0bd3ed0015318c3ab
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2016-08-07 15:13:31 +0200

    Removed static modifier on free math funcs

--- src/phiMath.cpp
+++ src/phiMath.cpp
@@ -1,1 +1,19 @@
 #include "phiMath.h"
+
+namespace Phoenix::Math
+{
+	Matrix4 view(const Vec3& cameraPos, const Vec3& lookAt, const Vec3& up)
+	{
+		return Matrix4{};
+	}
+
+	Matrix4 perspective(float horizontalFOV, float aspectRatio, float near, float far)
+	{
+		return Matrix4{};
+	}
+
+	Matrix4 ortho()
+	{
+		return Matrix4{};
+	}
+}
--- src/phiMath.h
+++ src/phiMath.h
@@ -5,7 +5,7 @@
 
 namespace Phoenix::Math
 {
-	static Matrix4 view(const Vec3& cameraPos, const Vec3& lookAt, const Vec3& up);
-	static Matrix4 perspective(float horizontalFOV, float aspectRatio, float near, float far);
-	static Matrix4 ortho();
+	Matrix4 view(const Vec3& cameraPos, const Vec3& lookAt, const Vec3& up);
+	Matrix4 perspective(float horizontalFOV, float aspectRatio, float near, float far);
+	Matrix4 ortho();
 }
