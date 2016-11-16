9cac539e417a3bcfb84861067ba3cb7338a91e3f
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2016-11-16 18:11:51 +0100

    add geometry classes

---
+++ src/Plane.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Vec3.hpp"
+
+namespace Phoenix::Math
+{
+	class Plane
+	{
+	private:
+		
+	public:
+		Plane(const Vec3& p1, const Vec3& p2, const Vec3& p3);
+	};
+}
---
+++ src/Ray.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Vec3.hpp"
+
+namespace Phoenix::Math
+{
+	class Ray
+	{
+	private:
+		Vec3 m_position;
+	};
+}
