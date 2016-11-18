c9702c4c7f94553dbad0ff9b0ca0b30f355ac049
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2016-11-18 16:25:19 +0100

    add pointAt(t)

---
+++ src/Math/Ray.cpp
@@ -0,0 +1,17 @@
+#include "Ray.hpp"
+
+namespace Phoenix::Math
+{
+	Ray::Ray(const Vec3& position, const Vec3& direction)
+		: m_origin(position)
+		, m_direction(direction)
+	{
+		m_direction.normalize();
+	}
+
+
+	Vec3 Ray::pointAt(float t) const
+	{
+		return m_origin + (m_direction * t);
+	}
+}
--- src/Math/Ray.hpp
+++ src/Math/Ray.hpp
@@ -7,6 +7,12 @@ namespace Phoenix::Math
 	class Ray
 	{
 	private:
-		Vec3 m_position;
+		Vec3 m_origin;
+		Vec3 m_direction;
+
+	public:
+		Ray(const Vec3& position, const Vec3& direction);
+
+		Vec3 pointAt(float t) const;
 	};
 }
