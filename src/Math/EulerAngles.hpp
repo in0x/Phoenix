66fa37ff80f9c9a64b02d3953ce52236416182fe
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2016-11-18 15:19:22 +0100

    introduce placeholder interface so that EulerAngles can be extended later

---
+++ src/Math/EulerAngles.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "Vec3.hpp"
+
+namespace Phoenix::Math
+{
+	// Placeholder interface incase the need for a dedicated 
+	// Euler angles representation arises later (i.e. to limit
+	// the angles to the canonical set).
+	typedef Vec3 EulerAngles;
+}
