c8b116adbe7235dba3345b5a5297c461498a027e
Philipp Welsch <pwelsch86@gmail.com>
2017-05-06 23:28:39 +0200

    convert to winmain

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <cassert>
-
-#include "Tests/MathTests.hpp"
+#include <Windows.h>
 #include "Core/obj.hpp"
+#include "Tests/MathTests.hpp"
+#include "Core\Win32Window.hpp"
 
 char* getCMDOption(char** start, char** end, const std::string& option)
 {
@@ -15,7 +16,16 @@ char* getCMDOption(char** start, char** end, const std::string& option)
 	return nullptr;
 }
 
-int main(int argc, char** argv)
+/*
+Likely structure:
+Window -> Interface class with game
+Win32Window -> Takes care of Windows specific stuff
+and is created on start up
+Engine -> actual engine class, gets a window passed to it
+on creation
+*/
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	using namespace Phoenix;
 
@@ -26,10 +36,26 @@ int main(int argc, char** argv)
 
 	std::unique_ptr<Mesh> fox = parseOBJ("Fox/", "RedFox.obj");
 	assert(fox != nullptr);
-
 	
-	int windowWidth = 1280;
-	int windowHeight = 720;
+	Phoenix::WindowConfig config = { 1600, 900,
+		0,0,
+		std::wstring(L"Phoenix"),
+		false };
+
+	Phoenix::Win32Window window(config);
+
+	if (!window.isOpen())
+	{
+		Phoenix::Logger::Error("Failed to initialize Win32Window");
+		return -1;
+	}
+
+	while (window.isOpen())
+	{
+		window.processMessages();
+	}
 
 	return 0;
 }
+
+
