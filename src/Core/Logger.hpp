fa4c8545aa9da63d69205b54c6115134d1819144
Philipp Welsch <pwelsch86@gmail.com>
2017-09-25 20:46:07 +0200

    attempt at logf

--- src/Core/Logger.hpp
+++ src/Core/Logger.hpp
@@ -14,6 +14,14 @@ namespace Phoenix
 		void warning(const std::string& msg);
 		void error(const std::string& msg);
 
+		//template <class... Args>
+		//void log(const char* format, Args... args)
+		//{
+		//	size_t bufferSize = snprintf(nullptr, 0, format, args...);
+		//	std::string buffer(bufferSize, ' ');
+		//	snprintf(buffer.data(), bufferSize + 1, format, args...);
+		//}
+
 #define S1(x) #x
 #define S2(x) S1(x)
 #define __LOCATION_INFO__ "In: " __FILE__ "\nAt: " S2(__LINE__) ", " __FUNCTION__ "() " 
