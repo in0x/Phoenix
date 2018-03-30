a7457d877ce0c8058bd6c4650d8663adc68d367b
Philipp Welsch <pwelsch86@gmail.com>
2018-03-30 20:17:56 +0100

    Start reworking ecs to reduce template usage

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,261 @@
 #include "Render/RIOpenGL/RIOpenGL.hpp"
 #include "Render/DeferredRenderer.hpp"
 
-#include "Core/World.hpp"
-#include "Components/StaticMeshComponent.hpp"
-#include "Components/MaterialComponent.hpp"
-#include "Components/TransformComponent.hpp"
-#include "Components/DirectionalLightComponent.hpp"
-#include "Components/DeferredRenderPrototype.hpp"
+#include "Core/Mesh.hpp"
+#include "Memory/ChunkArray.hpp"
+
+#include <unordered_map>
+#include <atomic>
+
+//namespace Phoenix
+//{
+	/*
+		Linear arrays of components
+		Deletion by pop and swap -> allows for linear runs over arrays
+		Entities -> ComponentIndex lookup map
+		Components have type ids
+		Register in world with type ids
+		Type ids used to lookup component arrays
+		Put the ID in an enum for now -> simple and centralized
+	*/
+
+//	namespace Components
+//	{
+//		enum Type
+//		{
+//			Transform,
+//			StaticMesh,
+//			Material,
+//			DirectionalLight,
+//			Camera,
+//			Max
+//		};
+//
+//		static_assert(Components::Max <= 64, "Currently only 64 unique component types are allowed");
+//	};
+//
+//	struct CCamera
+//	{
+//		CCamera(float horizontalFOV, float screenWidth, float screenHeight, float near, float far)
+//			: m_horizontalFOV(horizontalFOV)
+//			, m_screenWidth(screenWidth)
+//			, m_screenHeight(screenHeight)
+//			, m_near(near)
+//			, m_far(far)
+//		{}
+//
+//		Matrix4 getProjection()
+//		{
+//			return perspectiveRH(m_horizontalFOV, m_screenWidth / m_screenHeight, m_near, m_far);
+//		}
+//
+//		float m_horizontalFOV;
+//		float m_screenWidth;
+//		float m_screenHeight;
+//		float m_near;
+//		float m_far;
+//	};
+//
+//	struct CTransform
+//	{
+//		Vec3 m_translation;
+//		Vec3 m_scale;
+//		Vec3 m_rotation;
+//
+//		Matrix4 toMat4() const
+//		{
+//			return Matrix4::translation(m_translation.x, m_translation.y, m_translation.z)
+//				* Matrix4::rotation(m_rotation.x, m_rotation.y, m_rotation.z)
+//				* Matrix4::scale(m_scale.x, m_scale.y, m_scale.z);
+//		}
+//	};
+
+namespace Phoenix
+{
+	typedef uint64_t ComponentTypeId;
+	typedef uint64_t ComponentHandle;
+	typedef uint64_t EntityHandle; 
+	
+	uint64_t invalidHandle() // Consider bringing back Handle type.
+	{
+		return std::numeric_limits<uint64_t>::max();
+	}
+
+	struct ComponentTypeIdGenerator
+	{
+		static ComponentTypeId NextId()
+		{
+			return s_ComponentTypeIdCounter++;
+		}
+
+	private:
+		static std::atomic<ComponentTypeId> s_ComponentTypeIdCounter;
+	};
+
+	std::atomic<ComponentTypeId> ComponentTypeIdGenerator::s_ComponentTypeIdCounter = 0;
+
+	template <typename C>
+	ComponentTypeId getComponentTypeId()
+	{
+		static ComponentTypeId thisId = ComponentTypeIdGenerator::NextId();
+		return thisId;
+	}
+
+	struct Entity
+	{
+		bool hasComponent(ComponentTypeId type)
+		{
+			return m_components.find(type) != m_components.end();
+		}
+
+		std::unordered_map<ComponentTypeId, ComponentHandle> m_components;
+	};
+
+	struct World
+	{
+		~World()
+		{
+			for (ChunkArrayBase* allocator : m_componentAllocators)
+			{
+				delete allocator;
+			}
+		}
+
+		World(World&) = delete;
+		World(World&&) = delete;
+
+		template <typename C>
+		void registerComponentType(size_t initialCapacity = 64)
+		{
+			ComponentTypeId componentType = getComponentTypeId<C>();
+
+			if (m_componentAllocators.capacity() < componentType)
+			{
+				m_componentAllocators.resize(componentType);
+				ChunkArray<C>* pool = new ChunkArray<Comp>(initialCapacity);
+				m_componentPools.push_back(pool);
+			}
+		}
+
+		ComponentHandle getComponentHandle(ComponentTypeId componentType, EntityHandle entityId)
+		{
+			Entity& entity = m_entities[entityId];
+			
+			if (!entity.hasComponent(componentType))
+			{
+				return invalidHandle();
+			}
+
+			return entity.m_components.at(componentType);
+		}
+		
+		void* getComponent(ComponentTypeId componentType, EntityHandle entityId)
+		{
+			ComponentHandle handle = getComponentHandle(componentType, entityId);
+			
+			if (handle == invalidHandle())
+			{
+				return nullptr;
+			}
+
+			return m_componentAllocators[componentType]->at(handle);
+		}
+
+		void removeComponent(ComponentTypeId componentType, EntityHandle entityId)
+		{
+			// TODO: deregister from entity
+			ComponentHandle handle = getComponentHandle(componentType, entityId);
+			
+			if (handle == invalidHandle())
+			{
+				return;
+			}
+			
+			ChunkArrayBase* allocator = m_componentAllocators[componentType];
+			allocator->swapAndPop(handle);
+		}
+
+	private:
+		std::vector<Entity> m_entities;
+		std::vector<ChunkArrayBase*> m_componentAllocators;
+		uint64_t m_numEntities;
+	};
+	
+	class Component
+	{
+	public:
+		template <typename C>
+		C* sibling()
+		{
+			return m_owningWorld->getComponent(getComponentTypeId<C>(), m_owningEntity);
+		}
+
+	private:
+		World* m_owningWorld;
+		EntityHandle m_owningEntity;
+	};
+
+	template <typename C>
+	class ComponentIterator
+	{
+	public:
+		ComponentIterator(World* world);
+
+		ComponentIterator begin();
+		ComponentIterator end();
+
+		bool operator ==(const ComponentIterator<C>& other);
+		bool operator !=(const ComponentIterator<C>& other) {return !(*this == other)};
+
+		void operator++();
+
+		C& operator*();
+	};
+
+	class ISystem
+	{
+	public:
+		ISystem(World* world) 
+			: m_world(world)
+		{}
+		
+		virtual ~ISystem() {}
+		virtual void tick(float dt) = 0;
+	
+	protected:
+		World* m_world;
+	};
+
+	struct CDirectionalLight : public Component
+	{
+		Vec3 m_direction;
+		Vec3 m_color;
+	};
+
+	struct CStaticMesh : public Component
+	{
+		RenderMesh m_mesh;
+	};
+
+	struct CMaterial : public Component
+	{
+		Vec3 m_diffuse;
+		Vec3 m_specular;
+		float m_specularExp;
+	};
+
+	class DrawStaticMeshSystem : public ISystem
+	{
+		virtual void tick(float dt) override
+		{
+			for (CStaticMesh& mesh : ComponentIterator<CStaticMesh>(m_world))
+			{
+				CMaterial* material = mesh.sibling<CMaterial>();
+				// draw(mesh, material);
+			}
+		}
+	};
+}
 
 int main(int argc, char** argv)
 {
@@ -52,41 +301,13 @@ int main(int argc, char** argv)
 
 	renderInterface.setWindowToRenderTo(window);
 
-	World world;
+	// ECS Test
+	{
+		getComponentTypeId<CDirectionalLight>();
 
-	world.registerComponentType<TransformComponent>();
+		World world;
 
-	Entity::Id e = world.createEntity();
-	
-	world.addComponent<TransformComponent>(e);
-	world.removeComponent<TransformComponent>(e);
-
-	world.addSingletonComponent<DeferredRenderComponent>();
-
-	//WorldObject entity;
-	//entity.m_mesh = loadRenderMesh("Models/Fox/RedFox.obj", renderDevice);
-	//entity.m_material = Material(Vec3(1.f, 1.f, 1.f), Vec3(0.3f, 0.3f, 0.3f), 100.f);
-
-	//Matrix4 viewTf = lookAtRH(Vec3(0.f, 0.f, 7.f), Vec3(0.f, 0.f, 0.f), Vec3(0.f, 1.f, 0.f));
-	//Matrix4 projTf = perspectiveRH(70.f, (float)config.width / (float)config.height, 0.1f, 100.f);
-
-	//renderContext->setDepthTest(EDepth::Enable);
-
-	//DeferredRenderer renderer(renderDevice, config.width, config.height);
-	//renderer.setViewMatrix(viewTf);
-	//renderer.setProjectionMatrix(projTf);
-
-	//DirectionalLight lightRed;
-	//lightRed.m_direction = Vec3(0.f, -1.f, 0.f);
-	//lightRed.m_color = Vec3(0.3f, 0.f, 0.f);
-
-	//DirectionalLight lightBlue;
-	//lightBlue.m_direction = Vec3(1.f, 0.f, 0.f);
-	//lightBlue.m_color = Vec3(0.f, 0.f, 1.f);
-
-	//DirectionalLight lightGreen;
-	//lightGreen.m_direction = Vec3(0.f, 1.f, 0.f);
-	//lightGreen.m_color = Vec3(0.f, 1.f, 0.f);
+	}
 
 	while (!window->wantsToClose())
 	{
