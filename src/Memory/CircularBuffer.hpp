a474b288d689533f0b0f9669374a3dc1b9488b0d
Philipp Welsch <pwelsch86@gmail.com>
2018-05-15 23:41:36 +0100

    CircularBuffer fix

--- src/Memory/CircularBuffer.hpp
+++ src/Memory/CircularBuffer.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 namespace Phoenix
 {
 	template <typename T, size_t bufferSize>
@@ -9,49 +11,67 @@ namespace Phoenix
 		CircularBuffer()
 			: m_head(0)
 			, m_tail(0)
+			, m_bEmpty(true)
 		{}
 
 		void clear()
 		{
 			m_head = m_tail;
-		}
-
-		bool isFull()
-		{
-			return ((m_head + 1) % bufferSize) == m_tail;
+			m_bEmpty = true;
 		}
 
 		bool isEmpty()
 		{
-			return m_head == m_tail;
+			return m_bEmpty;
+		}
+
+		uint32_t size() const
+		{
+			return (m_head > m_tail) ? (m_head - m_tail) : (bufferSize - m_tail) + m_head;
+		}
+
+		uint32_t capacity() const
+		{
+			return bufferSize;
 		}
 
 		void add(const T& item)
 		{
-			m_data[m_head] = item;
-			m_head = (m_head + 1) % bufferSize;
-
 			if (m_head == m_tail)
 			{
 				m_tail = (m_tail + 1) % bufferSize;
 			}
+			
+			m_data[m_head] = item;
+			m_head = (m_head + 1) % bufferSize;
+		
+			m_bEmpty = false;
 		}
 
 		T* get()
 		{
-			if (isEmpty())
+			if (m_bEmpty)
 			{
 				return nullptr;
 			}
 
 			T* pItem = &m_data[m_tail];
 			m_tail = (m_tail + 1) % bufferSize;
+
+			m_bEmpty = (m_tail == m_head);
+
 			return pItem;
+		}
+
+		T* data()
+		{
+			return m_data;
 		}
 
 	private:
 		T m_data[bufferSize];
 		size_t m_head;
 		size_t m_tail;
+		bool m_bEmpty;
 	};
 }
